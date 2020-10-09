dc6f72d2587953217f7576fbec77a6178181a206
andrepputra <andre.putra@tokopedia.com>
2020-10-09 16:54:47 +0700

    implement big cache

--- main.go
+++ main.go
@@ -5,14 +5,26 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/allegro/bigcache"
 )
 
+var cache *bigcache.BigCache
+
 func github(w http.ResponseWriter, r *http.Request) {
+	cacheResp, err := getFromCache()
+	if err == nil {
+		json.NewEncoder(w).Encode(string(cacheResp))
+	}
+
 	resp, err := http.Get("https://api.github.com/status")
 	if err != nil {
 		json.NewEncoder(w).Encode(err)
 	}
 	data, _ := ioutil.ReadAll(resp.Body)
+
+	// setToCache(data)
 	resp.Body.Close()
 	json.NewEncoder(w).Encode(string(data))
 }
@@ -22,6 +34,24 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func getFromCache() ([]byte, error) {
+	data, err := cache.Get("github")
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return data, nil
+}
+
+func setToCache(data []byte) {
+	cache.Set("github", data)
+}
+
+func initCache() {
+	cache, _ = bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
+}
+
 func main() {
+	initCache()
 	handleRequests()
 }
