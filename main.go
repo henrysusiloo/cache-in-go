2f196a48d71e3c255c91d2d7fb94e1ad69721d74
Imam Rahensa <imam.rahensa@tokopedia.com>
2020-10-09 16:38:22 +0700

    cache implementation

--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/allegro/bigcache"
 )
 
-func github(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://api.github.com/status")
-	if err != nil {
-		json.NewEncoder(w).Encode(err)
-	}
-	data, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	json.NewEncoder(w).Encode(string(data))
+const cacheKey = "my-cache-key"
+
+var cache *bigcache.BigCache
+
+func main() {
+	cache, _ = bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
+	fmt.Println("running")
+	handleRequests()
 }
 
 func handleRequests() {
@@ -22,6 +26,21 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
-func main() {
-	handleRequests()
+func github(w http.ResponseWriter, r *http.Request) {
+	entry, err := cache.Get(cacheKey)
+	if err == nil {
+		fmt.Println("get from cache")
+		json.NewEncoder(w).Encode(string(entry))
+		return
+	}
+
+	resp, err := http.Get("https://api.github.com/status")
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	data, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	cache.Set(cacheKey, []byte(data))
+	json.NewEncoder(w).Encode(string(data))
 }
